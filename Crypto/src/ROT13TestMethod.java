c5e74c6324ed538cd68527e2f6b0d7d6c325159e
David <davidsamcun@gmail.com>
2022-03-26 13:29:10 -0400

    create method

---
+++ Crypto/src/ROT13TestMethod.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ROT13TestMethod {
+
+    @org.junit.jupiter.api.Test
+    void crypt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void encrypt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void decrypt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void rotate() {
+    }
+}
